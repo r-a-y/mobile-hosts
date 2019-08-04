0c057e4704bd02b03cdd38b8fc187fd946ac289c
r-a-y <r-a-y@gmx.com>
2019-08-04 10:37:46 -0700

    Converter: Strip 'object-subrequest' and 'subdocument' content types.

--- converter.php
+++ converter.php
@@ -61,7 +61,7 @@ foreach ( $lists as $name => $list ) {
 
 		// Replace filter syntax with HOSTS syntax.
 		// @todo Perhaps skip $third-party, $image and $popup?
-		$filter = str_replace( array( '||', '^', '$third-party', ',third-party', '$image', ',image', ',important', '$script', ',script', ',object', '$popup', '$empty' ), '', $filter );
+		$filter = str_replace( array( '||', '^', '$third-party', ',third-party', '$image', ',image', ',important', '$script', ',script', ',object', '$popup', '$empty', '$object-subrequest', '$subdocument' ), '', $filter );
 
 		// Skip rules matching 'xmlhttprequest' for now.
 		if ( false !== strpos( $filter, 'xmlhttprequest' ) ) {
